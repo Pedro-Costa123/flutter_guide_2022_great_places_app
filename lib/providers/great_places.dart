a4672a2997a3c97656419ef8ca20590b5ea5653c
Pedro-Costa123 <pedrocostaalves@live.com.pt>
2022-10-03 18:26:53 +0100

    Section 13 : 304

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 import '../models/place.dart';
+import '../helpers/db_helper.dart';
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
@@ -11,10 +12,7 @@ class GreatPlaces with ChangeNotifier {
     return [..._items];
   }
 
-  void addPlace(
-    String pickedTitle,
-    File pickedImage,
-  ) {
+  void addPlace(String pickedTitle, File pickedImage) {
     final newPlace = Place(
       id: DateTime.now().toString(),
       image: pickedImage,
@@ -22,6 +20,29 @@ class GreatPlaces with ChangeNotifier {
       location: null,
     );
     _items.add(newPlace);
+    notifyListeners();
+    DBHelper.insert(
+      'user_places',
+      {
+        'id': newPlace.id,
+        'title': newPlace.title,
+        'image': newPlace.image.path
+      },
+    );
+  }
+
+  Future<void> fetchAndSetPlaces() async {
+    final dataList = await DBHelper.getData('user_places');
+    _items = dataList
+        .map(
+          (item) => Place(
+            id: item['id'],
+            title: item['title'],
+            image: File(item['image']),
+            location: null,
+          ),
+        )
+        .toList();
     notifyListeners();
   }
 }
